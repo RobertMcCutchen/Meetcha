27dd2dc0c744e13c350163f3ba2bfdb8985b3fce
Robert McCutchen <rmccutchen2009@gmail.com>
2019-10-02 21:01:49 -0500

    Navigation

--- meetcha/App.js
+++ meetcha/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+
+import PlacesNavigator from './navigation/PlacesNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Meetcha!</Text>
-      <Button title="Change Text"/>
-    </View>
-  );
+    <PlacesNavigator />
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: 'yellow',
-    fontSize: 90,
-    fontStyle: "italic"
-  }
-});
